99ebc976ae9ea25b8d86c09dcebaf304e5bd3ce5
Jan Černý <jc@ucw.cz>
2025-07-15 22:18:47 +0200

    implement strip

--- _plugins/fancy-tags.rb
+++ _plugins/fancy-tags.rb
@@ -20,7 +20,25 @@ module Jekyll
     end
   end
 
+  class Strip < Liquid::Block
+    def render(context)
+      @md_converter = context.registers[:site].find_converter_instance(::Jekyll::Converters::Markdown)
+      content = super.to_s.strip
+      lines = content.split("\n").reject(&:empty?)
+      fraction = 1.0 / lines.length.to_f
+      output = '<div class="strip">'
+      lines.each_with_index do |line, index|
+        parsed_line = @md_converter.convert(line.strip)
+        parsed_line = parsed_line.gsub(/^<p>/, "").gsub(/<\/p>$/, "")
+        output += %Q[<div class="strip-line">#{parsed_line}</div>]
+      end
+      output += "</div>"
+      return output
+    end
+  end
+
+
 end
 
 Liquid::Template.register_tag("details", Jekyll::Details)
-
+Liquid::Template.register_tag("strip", Jekyll::Strip)
